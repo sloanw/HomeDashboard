7468143d3f1d60feecc891537cb6437d957db514
Sloan Whitney <swhitney@efront.com>
2018-03-25 00:26:24 -0400

    first pass at custom weather-item element

--- js/weather/weather.js
+++ js/weather/weather.js
@@ -5,10 +5,51 @@ var Weather = (function () {
 		let container = document.getElementById("weather");
 
 		if (weather_data && weather_data.currently) {
-			container = document.getElementById("weather");
-			container.innerText = weather_data.currently.apparentTemperature;
+			var period = weather_data.currently;
+			var html = `<weather-item temp="${period.apparentTemperature}" icon="${period.icon}" />`;
+			var node = document.createRange().createContextualFragment(html);
+			container.appendChild(node);
 		}
 	}
+
+	class WeatherElement extends HTMLElement {
+		constructor() {
+			super();
+
+			var shadow = this.attachShadow({ mode: 'open' });
+			var css = document.createElement('link');
+			css.setAttribute('rel', 'stylesheet');
+			css.setAttribute('href', 'css/weather-icons.css');
+			shadow.appendChild(css);
+
+			var container = document.createElement('div');
+			container.setAttribute('class', 'forecast');
+
+			for (i = 0; i < this.attributes.length; i++) {
+				let attr = this.attributes[i];
+
+				let node = document.createElement('div');
+
+				switch (attr.name) {
+					case 'icon':
+						node.setAttribute('class', `wi ${attr.value}`);
+						break;
+					default:
+						node.setAttribute('class', attr.name);
+						node.innerText = attr.value;
+				}
+
+				container.appendChild(node);
+			}
+
+			var style = document.createElement('style');
+			style.textContent = ``;
+
+			shadow.appendChild(style);
+			shadow.appendChild(container);
+		}
+	}
+	customElements.define('weather-item', WeatherElement);
 
 	return {
 		Init: function () {
