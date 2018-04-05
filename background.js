b02de8eed7aba23361307c9b2d5691697cefe28a
Sloan Whitney <swhitney@efront.com>
2018-04-05 09:13:10 -0400

    Action on click now opens new windows (from tab)

--- background.js
+++ background.js
@@ -1,3 +1,9 @@
 chrome.browserAction.onClicked.addListener(function() {
-	chrome.tabs.create({url: 'index.html'});
+	// chrome.tabs.create({url: 'index.html'});
+	chrome.windows.create({
+		url: 'index.html',
+		type: 'popup',
+		width: 500,
+		height: 330
+	});
 });
